ae8cae4f868f0848f9bf80047b99e652c3271aff
hashrambo <hashrambo@gmail.com>
2017-03-28 23:59:44 -0400

    Added Fib bottom up DP solution

--- src/main/java/DP/Fibonacci.java
+++ src/main/java/DP/Fibonacci.java
@@ -14,7 +14,7 @@ public class Fibonacci {
      * @param mem
      * @return
      */
-    public static int fib(int n, int [] mem) {
+    private static int fib(int n, int [] mem) {
 
         if (n == 0)
             return 0;
@@ -35,7 +35,7 @@ public class Fibonacci {
      * @param n
      * @return
      */
-    public static int fib(int n) {
+    private static int fib(int n) {
         if (n == 0)
             return 0;
         else if (n == 1)
@@ -45,11 +45,41 @@ public class Fibonacci {
     }
 
 
+    /**
+     * Bottom up dynamic programming iterative solution.
+     * Exact same computation.
+     * Topological sort of subproblem dependency DAG.
+     *
+     *             +-------------------v
+     * F_n-3 --> F_n-2 --> F_n-1 --> F_n
+     *  +---------------------^
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     * @param n
+     * @return
+     */
+    private static int fibBottomUp(int n) {
+
+        int fib [] = new int [n];
+
+        for (int i=0; i<n; i++) {
+            if (i < 2)
+                fib[i] = 1;
+            else {
+                fib [i] = fib[i - 1] + fib[i - 2];
+            }
+        }
+        return fib[n-1];
+    }
+
+
     public static void main(String[] args) {
         System.out.println(fib(10));
 
         final int [] mem = new int[11];
         System.out.println(fib(10, mem));
+
+        System.out.println(fibBottomUp(10));
 
     }
 }
